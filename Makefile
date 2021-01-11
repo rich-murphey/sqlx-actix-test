2c1cdfed0356acff7bdb22bb4e8a35a807f1bed4
rich-murphey <rich@murphey.org>
2021-01-11 07:31:45 -0600

    add a test to validate response payload. See wrk target in Makefile

--- Makefile
+++ Makefile
@@ -14,3 +14,8 @@ bench:
 hey:
 	hey -n 10000 -c 64 -m POST -H "Content-Type: application/json" -d '{"offset":0,"limit":256}' http://127.0.0.1:8080/junk
 	hey -n 10000 -c 64 -m POST -H "Content-Type: application/json" -d '{"offset":0,"limit":256}' http://127.0.0.1:8080/junkstream
+
+# warn up using drill, then switch to wrk to validate each response length
+wrk:
+	drill -q --benchmark tests/warmup.yml
+	wrk -c200 -t24 -d20s -s tests/junkstream.lua http://127.0.0.1:8080
--- tests/junkstream.lua
+++ tests/junkstream.lua
@@ -1,12 +1,12 @@
 --- -*- compile-command: "wrk -c200 -t24 -d20s -s junkstream.lua http://127.0.0.1:8080"; -*-
 wrk.method  = "POST"
-wrk.path    = "/junkstream"
+wrk.path    = "/junk"
 wrk.body    = "{\"offset\":0,\"limit\":100}"
 wrk.headers["Content-Type"] = "application/json;charset=utf-8"
 
 function response(status, headers, body)
    len = string.len(body)
-   if len ~= 2684983 then
-      print(len)
+   if status == 200 and len ~= 86393 then
+      print("invalid response length:", len, status)
    end
 end
