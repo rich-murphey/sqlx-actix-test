8ce3047a1647b02fdca6ba8e7e935f28afcb0dac
rich-murphey <rich@murphey.org>
2021-01-11 07:35:26 -0600

    add a test to validate response payload. See wrk target in Makefile

--- tests/junkstream.lua
+++ tests/junkstream.lua
@@ -1,6 +1,6 @@
 --- -*- compile-command: "wrk -c200 -t24 -d20s -s junkstream.lua http://127.0.0.1:8080"; -*-
 wrk.method  = "POST"
-wrk.path    = "/junk"
+wrk.path    = "/junkstream"
 wrk.body    = "{\"offset\":0,\"limit\":100}"
 wrk.headers["Content-Type"] = "application/json;charset=utf-8"
 
